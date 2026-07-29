8aa3b6dbfa6b5b2b869bd4ef6b368093f838e8f8
Gerardo Aguilar <gera_1501@hotmail.com>
2026-07-29 15:24:19 -0600

    feat(auth): implement JWT login endpoint

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import LoginSerializer
+from .services import AuthenticationService
+
+class LoginView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        serializer = LoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+
+        data = AuthenticationService.login(user)
+
+        return Response(data, status=status.HTTP_200_OK)
