cfe9dde758b8b7e8118c2ff2b85e61916619af28
Gerardo Aguilar <gera_1501@hotmail.com>
2026-08-23 01:22:57 -0600

    refactor(api): standardize purchase endpoints

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import LoginView, MeView, LogoutView, AdminTestView, RegisterView
+from .views import LoginView, MeView, LogoutView, RegisterView
 
 urlpatterns = [
     path("login/", LoginView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
     path("me/", MeView.as_view()),
     path("logout/", LogoutView.as_view()),
-    path("admin-test/", AdminTestView.as_view()),
     path("register/",RegisterView.as_view(),name="register"),
 ]
